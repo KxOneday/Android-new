5275fa34049ff6a2a1c286c6cd90132a74614c53
Fixer <fixer@example.com>
2026-06-30 11:25:29 +0800

    Remove testing artifacts

--- .check/MainActivity2.kt
+++
@@ -1,8 +0,0 @@
-package com.kxoneday.jinianri
-
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Settings
-
-fun test() {
-    val icon = Icons.Default.Settings
-}
